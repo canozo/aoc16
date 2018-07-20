280ed7f6ac9fd5ef5a1244b7bb9e7d54b7336c17
canozo <canwoahzo@gmail.com>
2018-07-20 17:00:43 -0600

    idk

--- day22/main.py
+++ day22/main.py
@@ -1,32 +1,80 @@
-d = open('input.txt')
-database = d.read().splitlines()
-d.close()
-nodes = []
+def process_list(puzzle):
+    nodes = []
+    for line in puzzle:
+        cord, size, used, avail, perc = line.strip().split()
 
-for i in range(2, len(database)):
-    cord, size, used, avail, perc = database[i].strip().split()
+        xi = cord.index('x')
+        yi = cord.index('y')
+
+        x = int(cord[xi + 1:yi - 1])
+        y = int(cord[yi + 1:])
+
+        items = (size, used, avail, perc)
+        size, used, avail, perc = (int(x[:-1]) for x in items)
+
+        nodes.append((x, y, size, used, avail))
+    return nodes
+
+
+def count_viable_nodes(database):
+    count = 0
+    for i in range(len(database)):
+        for j in range(len(database)):
+            if i == j:
+                continue
+
+            a = database[i]
+            b = database[j]
+            if a[3] != 0 and a[3] <= b[-1]:
+                count += 1
+    return count
+
+
+def process_grid(puzzle):
+    # find max x and y
+    cord = puzzle[-1].strip().split()[0]
 
     xi = cord.index('x')
     yi = cord.index('y')
 
-    x = int(cord[xi + 1:yi - 1])
-    y = int(cord[yi + 1:])
+    max_x = int(cord[xi + 1:yi - 1])
+    max_y = int(cord[yi + 1:])
 
-    size, used, avail, perc = (int(x[:-1]) for x in (size, used, avail, perc))
+    grid = [[None for _ in range(max_x + 1)] for _ in range(max_y + 1)]
 
-    nodes.append((x, y, size, used, avail))
+    for line in puzzle:
+        cord, size, used, avail, perc = line.strip().split()
 
-count = 0
+        xi = cord.index('x')
+        yi = cord.index('y')
 
-for i in range(len(nodes)):
-    for j in range(len(nodes)):
-        if i == j:
-            continue
+        x = int(cord[xi + 1:yi - 1])
+        y = int(cord[yi + 1:])
 
-        a = nodes[i]
-        b = nodes[j]
-        if a[3] != 0 and a[3] <= b[-1]:
-            count += 1
+        items = (size, used, avail, perc)
+        size, used, avail, perc = (int(x[:-1]) for x in items)
 
-print(count)
+        grid[y][x] = (size, used, avail)
+    return grid
 
+
+def solve(grid):
+    max_y, max_x = len(grid), len(grid[0])
+
+
+def main():
+    part = int(input('part: '))
+
+    with open('input.txt', 'r') as puzzle_file:
+        puzzle = puzzle_file.read().splitlines()
+
+    if part == 1:
+        database = process_list(puzzle)
+        print(count_viable_nodes(database))
+    elif part == 2:
+        grid = process_grid(puzzle)
+        print(solve(grid))
+
+
+if __name__ == '__main__':
+    main()
